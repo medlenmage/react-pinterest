50ce563da6ced984dde4b9fe4a90e76a0ea32ab7
Joshua Medlen <joshua246@gmail.com>
2020-08-17 20:49:01 -0500

    started navbar. got auth working

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnection from '../helpers/data/connection';
+import MyNavbar from '../components/myNavBar/MyNavBar';
+// import Auth from '../components/Auth/Auth';
+import BoardContainer from '../components/BoardContainer/BoardContainer';
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      if (authed) {
+        return <BoardContainer />;
+      }
+
+      return '';
+    };
+
     return (
       <div className="App">
+        <MyNavbar />
         <h2>React Pinterest</h2>
-        <button className="btn btn-info">I am a button</button>
+        {loadComponent()}
       </div>
     );
   }
